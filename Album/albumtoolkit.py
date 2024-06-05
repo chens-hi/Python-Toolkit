0e665fec9e036c5564690b6ac25b7a05c90b666c
chens-hi <chens_hi@icloud.com>
2024-06-05 17:58:42 +0800

    相册工具箱

---
+++ Album/albumtoolkit.py
@@ -0,0 +1,133 @@
+import os
+import stat
+import time
+import shutil
+import argparse
+import ffmpeg
+import filetype
+import exifread
+
+
+def get_album_time(file_path):
+    """
+    获取相册时间(优先拍摄时间, 默认修改时间)
+    :param file_path: 文件路径
+    :return: 时间戳
+    """
+
+    if os.path.isfile(file_path):
+        if filetype.is_image(file_path):
+            with open(file_path, mode="rb") as reader:
+                meta_object = exifread.process_file(reader)
+                if meta_object is not None:
+                    meta_time = meta_object.get("EXIF DateTimeOriginal")
+                    if meta_time is not None:
+                        create_time = time.strptime(str(meta_time), "%Y:%m:%d %H:%M:%S")
+                        return time.mktime(create_time)
+                    else:
+                        return os.path.getmtime(file_path)
+
+            return os.path.getmtime(file_path)
+        elif filetype.is_video(file_path):
+            meta_object = ffmpeg.probe(file_path)
+            if meta_object is not None:
+                meta_format = meta_object.get("format")
+                if meta_format is not None:
+                    meta_tags = meta_format.get("tags")
+                    if meta_tags is not None:
+                        meta_time = meta_tags.get("creation_time")
+                        if meta_time is not None:
+                            create_time = time.strptime(str(meta_time), "%Y-%m-%dT%H:%M:%S.%fZ")
+                            return time.mktime(create_time) - time.timezone
+                        else:
+                            return os.path.getmtime(file_path)
+        else:
+            return 0.0
+
+    return 0.0
+
+
+def organize_album_file(file_path, album_dir, move_mode=False, coverage_mode=False):
+    """
+    整理相册文件
+    :param file_path: 文件路径
+    :param album_dir: 相册目录
+    :param move_mode: 移动模式
+    :param coverage_mode: 覆盖模式
+    :return: 状态
+    """
+
+    time_stamp = get_album_time(file_path)
+    if time_stamp <= 0.0:
+        return False
+
+    time_object = time.localtime(time_stamp)
+    time_format = time.strftime("%Y:%m:%d %H:%M:%S", time_object)
+    time_path = os.path.join(album_dir, f"{time_object.tm_year:04}", f"{time_object.tm_mon:02}")
+    if not os.path.isdir(time_path):
+        os.makedirs(time_path, exist_ok=True)
+    os.chmod(time_path, stat.S_IRWXG)
+
+    full_name = os.path.basename(file_path)
+    file_name, file_extension = os.path.splitext(full_name)
+    target_path = os.path.join(time_path, f"{file_name}{file_extension}")
+
+    if coverage_mode:
+        try:
+            if os.path.isfile(target_path):
+                os.remove(target_path)
+        except OSError as error:
+            print(error)
+        else:
+            print(f"{time_format} {file_path} -> {target_path}")
+            if move_mode:
+                os.rename(file_path, target_path)
+            else:
+                shutil.copy2(file_path, target_path)
+    else:
+        count = 0
+        while True:
+            if count > 0:
+                target_path = os.path.join(time_path, f"{file_name}_{count}{file_extension}")
+            if os.path.isfile(target_path):
+                count += 1
+            else:
+                break
+        print(f"{time_format} {file_path} -> {target_path}")
+        if move_mode:
+            os.rename(file_path, target_path)
+        else:
+            shutil.copy2(file_path, target_path)
+
+    return True
+
+
+def organize_album_folder(organize_dir, album_dir, move_mode=False, coverage_mode=False):
+    """
+    整理相册文件
+    :param organize_dir: 整理目录
+    :param album_dir: 相册目录
+    :param move_mode: 移动模式
+    :param coverage_mode: 覆盖模式
+    :return: 状态
+    """
+
+    try:
+        if os.path.isdir(organize_dir):
+            for o in os.scandir(organize_dir):
+                if o.is_file():
+                    organize_album_file(o.path, album_dir, move_mode, coverage_mode)
+    except KeyboardInterrupt:
+        print("用户取消操作!")
+    else:
+        print("整理目录完成!")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="将指定目录下的相册文件, 按年月归纳整理到不同文件夹里!")
+    parser.add_argument("--wait_organize_folder", required=True, type=str, help="等待处理的目录")
+    parser.add_argument("--album_storage_folder", required=True, type=str, help="相册存放的目录")
+    parser.add_argument("--copy_or_move", required=False, type=bool, default=False, help="True: 拷贝到相册中 False: 移动到相册中")
+    parser.add_argument("--coverage_or_backup", required=False, type=bool, default=False, help="True: 覆盖同名相册文件 False: 保留相册同名文件")
+    args = parser.parse_args()
+    organize_album_folder(args.wait_organize_folder, args.album_storage_folder, args.copy_or_move, args.coverage_or_backup)
